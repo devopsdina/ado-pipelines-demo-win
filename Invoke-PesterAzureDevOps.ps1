e1dfb182188fd4c51bc047d9eca12e485f2af604
Dina Muscanell <dina.muscanell@allscripts.com>
2019-03-11 11:31:35 -0400

    Trying version 4.0.8 per https://github.com/rfennell/AzurePipelines/issues/243

--- Invoke-PesterAzureDevOps.ps1
+++ Invoke-PesterAzureDevOps.ps1
@@ -1,7 +1,7 @@
 # Used for Azure DevOps unit test results
 # To run locally, simply just run Invoke-Pester, no need to run this script
 
-Import-Module Pester -Version 3.4.0
+Import-Module Pester -Version 4.0.8
 
 $outputFile = ".\TEST-RESULTS.xml"
 
