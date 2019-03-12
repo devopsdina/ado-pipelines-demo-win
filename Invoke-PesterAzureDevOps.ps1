e44b5b3b6d6ecb255e8000eecc40aaf473590403
Dina Muscanell <dina.muscanell@allscripts.com>
2019-03-12 08:50:39 -0400

    removing pester import all together to see what happens

--- Invoke-PesterAzureDevOps.ps1
+++ Invoke-PesterAzureDevOps.ps1
@@ -1,8 +1,6 @@
 # Used for Azure DevOps unit test results
 # To run locally, simply just run Invoke-Pester, no need to run this script
 
-Import-Module Pester -Version 4.0.8
-
 $outputFile = ".\TEST-RESULTS.xml"
 
 Invoke-Pester -OutputFile $outputFile -OutputFormat NUnitXml
